196e4a6685da5ea200bdd2cbc487b7f14d0e9105
Md Shahadat Hossain <mdridoysikder862@gmail.com>
2025-07-06 16:42:44 +0600

    production api url added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://library-management-system-phi-six.vercel.app/api",
   }),
   tagTypes: ["Book", "Borrow"],
   endpoints: () => ({}),
